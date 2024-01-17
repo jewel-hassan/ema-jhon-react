051269b013bdcac2edd58527e2386ecb17beffdc
jewel hassan <jewelhassan399@gmail.com>
2024-01-17 14:20:19 +0600

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,15 @@ import logo from "../../images/Logo.svg";
 
 const Header = () => {
     return (
-        <div className="header">
+        <nav className="header">
            <img src={logo} alt="" srcset="" />
-           <a href="/shop">Shop</a>
+          <div>
+          <a href="/shop">Shop</a>
            <a href="/order">Order</a>
            <a href="/inverntory">Inverntory</a>
            <a href="/login">Login</a>
-        </div>
+          </div>
+        </nav>
     );
 };
 
